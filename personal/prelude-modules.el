661e82261f9f979f82de40351939aa4da1b2bad4
Matteo Banerjee <matteobanerjee@gmail.com>
2020-08-28 10:21:37 -0400

    Move ivy to the bottom to get the keybindings

--- personal/prelude-modules.el
+++ personal/prelude-modules.el
@@ -1,17 +1,9 @@
-;;; Uncomment the modules you'd like to use and restart Prelude afterwards
-
-(require 'prelude-ivy) ;; Interface for narrowing and search
-;; (require 'prelude-helm-everywhere) ;; Enable Helm everywhere
-(require 'prelude-company)
-
-;;; Programming languages support
 (require 'prelude-clojure)
 (require 'prelude-emacs-lisp)
 (require 'prelude-erlang)
 (require 'prelude-elixir)
 (require 'prelude-js)
 (require 'prelude-lisp)
-(require 'prelude-org)
 (require 'prelude-python)
 (require 'prelude-rust)
 (require 'prelude-scheme)
@@ -20,3 +12,7 @@
 (require 'prelude-css)
 (require 'prelude-xml)
 (require 'prelude-yaml)
+(require 'prelude-company)
+(require 'prelude-ivy)
+;; (require 'prelude-helm)
+;; (require 'prelude-helm-everywhere)
